e74d8d955f728ba642746b80dad9aea637416478
EugeneStarshov <yeenzo@yandex.ru>
2022-01-22 10:50:23 +0300

    protokol TaskProtocol class TaskModel

---
+++ toDoList/toDoList/Resourses/TaskModel.swift
@@ -0,0 +1,33 @@
+//
+//  TaskModel.swift
+//  toDoList
+//
+//  Created by Евгений Старшов on 22.01.2022.
+//
+
+import Foundation
+
+
+class MainTask: TaskProtokol {
+    
+    var name: String
+    
+    var description: String
+    
+    var status: TaskStatus
+    
+    init(name: String, description: String, status: TaskStatus) {
+        self.name = name
+        self.description = description
+        self.status = status
+    }
+    
+    func open() {
+        status = .opened
+    }
+    
+    func close() {
+        status = .closed
+    }
+}
+
